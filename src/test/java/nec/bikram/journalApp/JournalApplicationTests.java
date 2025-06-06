9b1a71c9f5ad040903204581cae194ca5d440c5e
proBIKRAM <lb9770099@gmail.com>
2025-06-06 19:27:28 +0545

    removing tests

--- src/test/java/nec/bikram/journalApp/JournalApplicationTests.java
+++ src/test/java/nec/bikram/journalApp/JournalApplicationTests.java
@@ -1,13 +1,13 @@
-package nec.bikram.journalApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JournalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package nec.bikram.journalApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JournalApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

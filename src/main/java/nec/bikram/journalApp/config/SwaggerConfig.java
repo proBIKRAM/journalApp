1c607720afa1b861760ba70320c58f3e27b8af8f
proBIKRAM <lb9770099@gmail.com>
2026-03-03 17:31:58 +0545

    added swagger configuration for API documentation

---
+++ src/main/java/nec/bikram/journalApp/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package nec.bikram.journalApp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI myCustomConfig() {
+        return new OpenAPI()
+                .info(new Info().title("Journal App APIs").description("By Bikram"))
+                .servers(List.of(
+                        new io.swagger.v3.oas.models.servers.Server().url("http://localhost:8080").description("Local"),
+                        new Server().url("http://localhost:8081").description("Live")
+                ));
+    }
+}
